5a36fca08ad7da09490a20c5fa030129d3ba8664
Rodrigo Salazar <rodrigo.salazar@utec.edu.pe>
2022-05-24 21:40:57 -0500

    Implementacion

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import random
 from abc import ABC, ABCMeta, abstractmethod
-
+from threading import Lock, Thread
 
 class SingletonMeta(ABCMeta):
     _instances = {}
+    _lock: Lock = Lock()
 
     def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
+        with cls._lock:
+            if cls not in cls._instances:
+                instance = super().__call__(*args, **kwargs)
+                cls._instances[cls] = instance
         return cls._instances[cls]
 
 
